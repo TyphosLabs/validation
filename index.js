967888343b3dbaea5c712ffff37921889e49e060
imaustink <austin@austinkurpuis.com>
2016-10-10 14:12:10 -0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+﻿/* compilation of validation modules for server-side */
+
+// start with the base
+var Validation = require('@orchardcorset/validation-base')();
+
+// add plugins
+require('@orchardcorset/validation-invalid')(Validation);
+require('@orchardcorset/validation-invalid-error')(Validation);
+require('@orchardcorset/validation-types')(Validation);
+require('@orchardcorset/validation-express')(Validation);
+
+// and export
+module.exports = Validation;
